ff505b56453cd18a02003218b17437c41b58f26d
machitake <glory.tm5@gmail.com>
2024-11-02 12:56:11 +0900

    ページ遷移した際にページ上部へ移動

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,13 +5,22 @@ export default function Pagination({ currentPage, totalPages, onPageChange }) {
   const handlePrevPage = () => {
     if (currentPage > 1) {
       onPageChange(currentPage - 1);
+      scrollToTop();
     }
   };
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
       onPageChange(currentPage + 1);
+      scrollToTop();
     }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   };
 
   return (
@@ -19,6 +28,6 @@ export default function Pagination({ currentPage, totalPages, onPageChange }) {
       <button onClick={handlePrevPage} disabled={currentPage === 1}>前へ</button>
       <span>ページ {currentPage} / {totalPages}</span>
       <button onClick={handleNextPage} disabled={currentPage === totalPages || totalPages === 0}>次へ</button>
-      </div>
+    </div>
   );
 }
